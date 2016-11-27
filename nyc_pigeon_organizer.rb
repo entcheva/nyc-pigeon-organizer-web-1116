d04051faa200a142d5bc222128c09e8bc6e50649
Christina Entcheva <christina.entcheva@gmail.com>
2016-11-27 23:04:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output = {}
+
+  data.each do |key1, val1|
+    val1.each do |key2, val2|
+      val2.each do |name|
+        output[name] ||= {}
+        output[name][key1] ||= []
+        output[name][key1] << key2.to_s
+      end
+    end
+  end
+  output
 end
