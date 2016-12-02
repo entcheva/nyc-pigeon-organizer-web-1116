7b46600a970667f31b9f280e801a48a66bc321c6
Christina Entcheva <christina.entcheva@gmail.com>
2016-12-02 18:11:34 -0500

    Update nyc_pigeon_organizer.rb

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,41 @@
-def nyc_pigeon_organizer(data)
-  output = {}
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
 
-  data.each do |key1, val1|
-    val1.each do |key2, val2|
-      val2.each do |name|
-        output[name] ||= {}
-        output[name][key1] ||= []
-        output[name][key1] << key2.to_s
-      end
-    end
-  end
-  output
+
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+
+  data[:gender].each do |key, value|
+   value.each do |name|
+     new_hash[name]= {:color => [],:gender => [], :lives =>[]}
+     end
+   end
+
+   data.each do |key, hash| # color, etc, hash
+     hash.each do |type, name| # purple, array
+       name.each do |pigeon| # name array, each name
+         if new_hash[pigeon]
+           new_hash[pigeon][key] << type.to_s
+         end
+       end
+     end
+   end
+
+  new_hash
 end
